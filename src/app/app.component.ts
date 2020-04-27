2a621a4980b97ef86973ee48b785d490cff09b34
Igor Vinicius <igorvinnicius@gmail.com>
2020-04-27 15:27:47 +0200

    passing recipe data with property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-recipe-book';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature
+  }
+
 }
